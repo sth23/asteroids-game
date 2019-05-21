cea0faf9d1e9f57df1dd304f4b21818dc0ca508a
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-20 20:01:23 -0400

    Updated from Brython Server: 5/20/2019 8:01:23 PM

--- asteroids-game.py
+++ asteroids-game.py
@@ -33,7 +33,8 @@ class BigAsteroid(Sprite):
         self.rotation = 0
         self.vx = self.speed * math.sin(self.rotation)
         self.vy = self.speed * math.cos(self.rotation)
-        self.vr = 0.1 * random.random()
+        self.vr = 0.05 * random.random()
+        self.fxcenter = self.fycenter = 0.5
         
     def step(self):
         self.x += self.vx
