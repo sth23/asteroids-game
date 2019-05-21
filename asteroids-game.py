7c9c24125fd43ba95a1f5b61a209e16d6a2b5890
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-20 22:13:44 -0400

    Updated from Brython Server: 5/20/2019 10:13:44 PM

--- asteroids-game.py
+++ asteroids-game.py
@@ -134,7 +134,7 @@ class Ship(Sprite):
         self.gamewidth = width
         self.gameheight = height
         self.speedlimit = 7.5
-        self.rotatespeed = 0.1
+        self.rotatespeed = 0.15
         self.maxspin = 1
         self.thrust = 0.1
         self.vx = 0
