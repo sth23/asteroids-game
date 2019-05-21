7266a718dc8a9ea06a2004b9ba5abdc5b09b3244
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-20 22:57:37 -0400

    Updated from Brython Server: 5/20/2019 10:57:37 PM

--- asteroids-game.py
+++ asteroids-game.py
@@ -161,27 +161,27 @@ class Ship(Sprite):
         AsteroidsGame.listenKeyEvent("keydown", "up arrow", self.thrustOn)
 
     def shoot(self, event):
-        if self.extralives > 0:
+        if self.extralives >= 0:
             Bullet((self.x - 15 * math.sin(self.rotation), self.y - 15 * math.cos(self.rotation)), self.rotation)
 
     def rotateRightOn(self, event):
-        if self.extralives > 0:
+        if self.extralives >= 0:
             self.vr = -self.rotatespeed
         
     def rotateRightOff(self, event):
-        if self.extralives > 0:
+        if self.extralives >= 0:
             self.vr = 0
         
     def rotateLeftOn(self, event):
-        if self.extralives > 0:
+        if self.extralives >= 0:
             self.vr = self.rotatespeed
         
     def rotateLeftOff(self, event):
-        if self.extralives > 0:
+        if self.extralives >= 0:
             self.vr = 0
         
     def thrustOn(self, event):
-        if self.extralives > 0:
+        if self.extralives >= 0:
             self.deltavx = -self.thrust * math.sin(self.rotation)
             self.deltavy = -self.thrust * math.cos(self.rotation)
         
@@ -227,7 +227,7 @@ class AsteroidsGame(App):
         self.player1.y = self.height / 2
         self.player1.vx = 0
         self.player1.vy = 0
-        self.rotaion = 0
+        self.player1.rotaion = 0
         self.player1.extralives -= 1
         self.showExtraLives()
         self.count = 0
